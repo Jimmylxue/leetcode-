ea1a013550b93fee360dd9a094d4c91d3d7a2e4e
jimmyxuexue <1002661758@qq.com>
2021-11-25 14:08:47 +0800

    73 删除链表重复项

---
+++ 题库/83-删除排序链表中的重复元素.js
@@ -0,0 +1,45 @@
+/**
+
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+
+返回同样按升序排列的结果链表。
+
+输入：head = [1,1,2]
+输出：[1,2]
+
+输入：head = [1,1,2,3,3]
+输出：[1,2,3]
+
+提示：
+
+链表中节点数目在范围 [0, 300] 内
+-100 <= Node.val <= 100
+题目数据保证链表已经按升序排列
+
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  var cur = head;
+  while (cur && cur.next) {
+    // 指针不断向后移动
+    if (cur.val == cur.next.val) {
+      // 相同 多挪动一位
+      cur.next = cur.next.next;
+    } else {
+      // 不同 挪动一位
+      cur = cur.next;
+    }
+  }
+  return head;
+};
